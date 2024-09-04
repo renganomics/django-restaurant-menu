64ee834f9ad53c9fd8951c02adc47b949b2ae820
renganomics <orengasi@gmail.com>
2024-09-04 13:01:23 +0100

    Created get_context_data method for MenuList class

--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -7,6 +7,11 @@ class MenuList(generic.ListView):
     queryset = Item.objects.order_by("-date_created")
     template_name = "index.html"
 
+    def get_context_data(self):
+        context = {"meals": ["Pizza", "Steak"],
+                   "ingredients": ["Cheese"]}
+        return context
+
 class MenuItemDetail(generic.DetailView):
     model = Item
     template_name = "menu_item_detail.html"
