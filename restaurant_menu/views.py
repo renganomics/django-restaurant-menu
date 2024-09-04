ca984857562218802c9ee0df025a235b4eb59fc9
renganomics <orengasi@gmail.com>
2024-09-04 12:44:13 +0100

    Created and connected views.py classes to restaurant_menu and mysite urls

--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Item
 
-# Create your views here.
+
+class MenuList(generic.ListView):
+    queryset = Item.objects.order_by("-date_created")
+    template_name = "index.html"
+
+class MenuItemDetail(generic.DetailView):
+    model = Item
+    template_name = "menu_item_detail.html"
