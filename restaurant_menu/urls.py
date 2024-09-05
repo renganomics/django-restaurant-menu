7e405b30ced16d0b025b4d7c4390e57f0fe9ab1a
renganomics <orengasi@gmail.com>
2024-09-06 00:42:14 +0100

    Added dynamic links for menu items

--- restaurant_menu/urls.py
+++ restaurant_menu/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.MenuList.as_view(), name='home')
+    path('', views.MenuList.as_view(), name='home'),
+    path('item/<int:pk>/', views.MenuItemDetail.as_view(), name='menu_item')
 ]
