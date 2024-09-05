028f1bb3161b342733482df53e185f535b3308a4
renganomics <orengasi@gmail.com>
2024-09-05 23:36:40 +0100

    Added for loops to display context data on home page

--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Item
+from .models import Item, MEAL_TYPE
 
 
 class MenuList(generic.ListView):
     queryset = Item.objects.order_by("-date_created")
     template_name = "index.html"
 
-    def get_context_data(self):
-        context = {"meals": ["Pizza", "Steak"],
-                   "ingredients": ["Cheese"]}
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["meals"] = MEAL_TYPE
         return context
 
 class MenuItemDetail(generic.DetailView):
